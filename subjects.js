11b558d4e9d749a90cb8ee9ff2166cfce7c598a8
RahulMG90 <61360857+RahulMG90@users.noreply.github.com>
2021-11-19 18:15:19 +0400

    Form validation and back to home button
    Form has a validation function to check the user's input and is able to press the checkout button. Another button is added in the checkout page to go back to the home page as the checkout button will be disabled after checking out.

---
+++ subjects.js
@@ -0,0 +1,97 @@
+let subjects = [
+    {
+        "id": 1001,
+        "title": "Literature",
+        "price": 50,
+        "location": "Leicester",
+        "img": "/images/pen.png",
+        "availableSlots": 5
+    },
+    {
+        "id": 1002,
+        "title": "Algebra",
+        "price": 80,
+        "location": "Chelsea",
+        "img": "/images/math.png",
+        "availableSlots": 5
+
+    },
+    {
+        "id": 1003,
+        "title": "Art",
+        "price": 40,
+        "location": "Lincoln",
+        "img": "/images/art.webp",
+        "availableSlots": 5
+
+    },
+    {
+        "id": 1004,
+        "title": "Drama",
+        "price": 110,
+        "location": "Birmingham",
+        "img": "/images/drama.jpg",
+        "availableSlots": 5
+
+    },
+
+    {
+        "id": 1005,
+        "title": "English Creative Writing",
+        "price": 95,
+        "location": "Liverpool",
+        "img": "/images/creative.jpg",
+        "availableSlots": 5
+
+    },
+    {
+        "id": 1006,
+        "title": "Music",
+        "price": 55,
+        "location": "Newcastle",
+        "img": "/images/music.jpg",
+        "availableSlots": 5
+
+    },
+
+    {
+        "id": 1007,
+        "title": "German Language",
+        "price": 84,
+        "location": "Liverpool",
+        "img": "/images/german.webp",
+        "availableSlots": 5
+
+    },
+
+    {
+        "id": 1008,
+        "title": "French Language",
+        "price": 80,
+        "location": "Bradford",
+        "img": "/images/french.jpg",
+        "availableSlots": 5
+
+    },
+
+    {
+        "id": 1009,
+        "title": "Martial Arts",
+        "price": 70,
+        "location": "Chelsea",
+        "img": "/images/martial.jpg",
+        "availableSlots": 5
+
+    },
+
+    {
+        "id": 1010,
+        "title": "Dance",
+        "price": 115,
+        "location": "Birmingham",
+        "img": "/images/dance.jpg",
+        "availableSlots": 5
+
+    },
+
+]
